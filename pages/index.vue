7704d6a8bced512cf46f7c2f60df2b06f6fe0be5
Marvin Marinas <mlmarinas@gmail.com>
2021-03-28 21:34:42 -0600

    Moved seo twitter tags on index.vue file (test deploy)

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <SocialHead
       title="Wellrefresh "
-      description="A Health and Wellness website aiming to help people with dieting, weightloss, and fitness."
+      description="A Health and Wellness website aiming to help people with dieting, weightloss, and fitness!"
       image="/twitter-seo-card.png"
     />
     <ArticleShowcase />
