372656cac1a4034e799dab3d26bd97dfc07b067a
Marvin Marinas <mlmarinas@gmail.com>
2021-04-06 22:21:04 -0600

    Upated favicon.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,37 @@ export default {
         name: 'description',
         content:
           'A Health and Wellness website aiming to help people with dieting, weightloss, and fitness.'
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff'
       }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       {
         rel: 'stylesheet',
         href:
