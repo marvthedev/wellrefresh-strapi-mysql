60abe2d40ea6f31e4844b72618e9cd5638046674
Marvin Marinas <mlmarinas@gmail.com>
2021-04-07 13:58:03 -0600

    Installed nuxt-frg-icon for favicon.

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,25 +23,6 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png'
-      },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-      {
         rel: 'stylesheet',
         href:
           'https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap'
@@ -70,14 +51,11 @@ export default {
 
   modules: [
     '@nuxtjs/apollo',
-    '@nuxtjs/axios',
-    '@nuxtjs/markdownit',
     '@nuxtjs/pwa',
-    '@nuxtjs/style-resources'
+    '@nuxtjs/style-resources',
+    'nuxt-rfg-icon',
+    '@nuxtjs/manifest'
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -104,10 +82,6 @@ export default {
         loadingKey: 'loading'
       }
     }
-  },
-
-  markdownit: {
-    runtime: true // Support `$md()`
   },
 
   env: {
